eb804a0d28389087ba495e3797fc53734a652ae7
GuillermoEAC <guillermochina12@gmail.com>
2025-12-09 00:38:22 -0700

    Paso 4 para el front

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,37 @@
+// import {
+//   ApplicationConfig,
+//   provideZonelessChangeDetection,
+// } from '@angular/core';
+// import { provideRouter, withComponentInputBinding } from '@angular/router';
+
+// import { routes } from './app.routes';
+// import {
+//   provideClientHydration,
+//   withEventReplay,
+// } from '@angular/platform-browser';
+// import { provideHttpClient, withFetch } from '@angular/common/http';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideZonelessChangeDetection(), // ¡Correcto!
+//     provideRouter(routes, withComponentInputBinding()),
+//     provideHttpClient(withFetch()),
+//     provideClientHydration(withEventReplay()),
+//   ],
+// };
+
 import {
   ApplicationConfig,
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-
 import { routes } from './app.routes';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZonelessChangeDetection(), // ¡Correcto!
+    provideZonelessChangeDetection(),
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withFetch()),
-    provideClientHydration(withEventReplay()),
   ],
 };
